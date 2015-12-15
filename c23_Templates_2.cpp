06b27ee4e1dd608f7da4acb380122d41056d3f48
uknowit <shashikumarhn@gmail.com>
2015-12-16 00:21:25 +0530

    Adding files

---
+++ c23_Templates_2.cpp
@@ -0,0 +1,7 @@
+#include "c23_Templates_2.hpp"
+
+int main() 
+{
+    return 0;
+}
+
---
+++ c23_Templates_2.hpp
@@ -0,0 +1,15 @@
+#ifndef _C23_TEMPLATES_2_HPP
+#define _C23_TEMPLATES_2_HPP
+
+#include <iostream>
+#include <string>
+#include <cstdio>
+
+template<typename T> class Matrix;
+template<typename T> class Vector
+{
+    T v[4];
+    public:
+        friend Vector operator* <>(const Matrix<T>&, const Vector&);
+};
+#endif
