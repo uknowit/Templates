339877adb0d33615fcb80d079188db43789c4b6b
uknowit <shashikumarhn@gmail.com>
2015-12-16 00:35:20 +0530

    Adding files

--- c23_Templates_2.hpp
+++ c23_Templates_2.hpp
@@ -10,6 +10,7 @@ template<typename T> class Vector
 {
     T v[4];
     public:
-        friend Vector operator* <>(const Matrix<T>&, const Vector&);
+        //friend Vector operator* <>(const Matrix<T>&, const Vector&);
+        template<typename T1> friend Vector<T1> operator* (const Matrix<T>&, const Vector&);
 };
 #endif
